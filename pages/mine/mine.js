e5c10bc7406caf773285344fb2db75ae8b30a894
Steve <zhoudongyu08@gmail.com>
2019-04-13 16:49:05 +0800

    用户上传头像功能

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -5,24 +5,24 @@ Page({
     faceUrl: "../resource/images/noneface.png",
   },
 
-  onLoad: function (params) {
+  onLoad: function(params) {
 
   },
 
-  logout: function () {
-    var user=app.userInfo;
+  logout: function() {
+    var user = app.userInfo;
     var serverUrl = app.serverUrl;
     wx.showLoading({
       title: '请等待...',
     })
     // 调用后端
     wx.request({
-      url: serverUrl + '/logout?userId='+user.id,
-      method: "POST",
+      url: serverUrl + '/logout?userId=' + user.id,
+      method: "GET",
       header: {
         'content-type': 'application/json' // 默认值
       },
-      success: function (res) {
+      success: function(res) {
         console.log(res.data);
         wx.hideLoading()
         if (res.data.status == 200) {
@@ -35,9 +35,51 @@ Page({
           // 页面跳转  
           wx.redirectTo({
             url: '../userLogin/login',
-          })       
+          })
         }
       }
     })
+  },
+
+  changeFace: function() {
+    console.log('11')
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['compressed'],
+      sourceType: ['album'],
+      success: function(res) {
+        var tempFilePaths = res.tempFilePaths;
+        console.log(tempFilePaths)
+        var serverUrl = app.serverUrl
+        wx.showLoading({
+          title: '上传中...',
+        })
+        wx.uploadFile({
+          url: serverUrl + '/user/uploadFace?userId=' + app.userInfo.id,
+          filePath: tempFilePaths[0],
+          name: 'file',
+          header: {
+            'content-type': 'application/json' // 默认值
+          },
+          success(res) {
+            const data = res.data
+            console.log(data)
+            wx.hideLoading()
+            if (res.data.status == 200) {
+              wx.showToast({
+                title: '上传成功',
+                icon: 'success',
+                duration: 2000
+              });
+            } else if (res.data.status == 200) {
+              wx.showToast({
+                title: res.data.msg
+              });
+            }
+          }
+        })
+      },
+    })
   }
+
 })
